d0164884c54f627625e67d7cf632ab0f20215edd
tarundanda147 <154943106+tarundanda147@users.noreply.github.com>
2024-03-04 14:00:50 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "ami.id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+  //default     = "ami-03bb6d83c60fc5f7c"
+}
+
+variable "instance_type" {
+  description   = "The type of EC2 instance"
+  type          = string
+  //default = "t2.micro"
+}
+
+variable "vpc_id" {
+  description = "the default VPC of the region"
+  type = string
+}
+variable "port_in" {
+  description = "SSH port value"
+  type = number
+}
+
+variable "port_out" {
+  description = "SSH port value"
+  type = number
+}
+variable "cidr_block" {
+  description = "The CIDR range"
+  type = string
+}
