57bc6e6a07c1c30bb382e43e239e83f9fbc561cb
tarundanda147 <154943106+tarundanda147@users.noreply.github.com>
2024-03-04 15:06:34 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,13 @@
-
+provider "aws" {
+    region = "ap-south-1"
+}
+ 
+terraform {
+    backend "s3" {
+      region = "ap_south-1"
+      bucket = "tarundanda147"
+      key = "state.tfstate"
+      dynamodb_table = "statelockfile"
+      encrypt = true
+    }
+}
