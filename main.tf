c431020fdc5fa6d340728a3a9ef9b3838506252e
tarundanda147 <154943106+tarundanda147@users.noreply.github.com>
2024-03-04 14:01:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
+resource "aws_instance" "ec2-instance" {
+        aim = var.ami.id
+        instance_type = var.instance_type
+        vpc_security_groups_ids = [aws_security_group.mysg.id]
+}
 
+resource "aws_security_group" "mysg" {
+        name = "ssh-port"
+        description = "Allow ssh traffic"
+        vpc_id = var.vpc_id
+        ingress {
+                description = "Allow ssh inbound traffic"
+                from_port = var.port_in
+                to.port = var.port_out
+                protocol = "tcp"
+                cidr_blocks = [var.cidr_block]
+}
+        tags = {
+                name ="allow_ssh"
+        }
+}
